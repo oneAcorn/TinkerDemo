747fba12edd21006c83c5c8be53a24914bbbbf38
苏飞 <fei.su@imcore.com>
2021-11-10 17:26:21 +0800

    init

---
+++ app/src/main/java/com/acorn/test/tinkerdemo/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.acorn.test.tinkerdemo
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.tencent.tinker.lib.tinker.TinkerInstaller
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        askForRequiredPermissions()
+
+        findViewById<android.view.View?>(R.id.loadPatchBtn)?.setOnClickListener {
+            TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(),
+                android.os.Environment.getExternalStorageDirectory()
+                    .getAbsolutePath() + "/patch_signed_7zip.apk"
+            )
+        }
+    }
+
+    private fun askForRequiredPermissions() {
+        if (Build.VERSION.SDK_INT < 23) {
+            return
+        }
+        if (!hasRequiredPermissions()) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                0
+            )
+        }
+    }
+
+    private fun hasRequiredPermissions(): Boolean {
+        return if (Build.VERSION.SDK_INT >= 16) {
+            val res = ContextCompat.checkSelfPermission(
+                this.applicationContext,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+            res == PackageManager.PERMISSION_GRANTED
+        } else {
+            // When SDK_INT is below 16, READ_EXTERNAL_STORAGE will also be granted if WRITE_EXTERNAL_STORAGE is granted.
+            val res = ContextCompat.checkSelfPermission(
+                this.applicationContext,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            res == PackageManager.PERMISSION_GRANTED
+        }
+    }
+}
---
+++ app/src/main/java/com/acorn/test/tinkerdemo/MyApplicationLike.java
@@ -0,0 +1,65 @@
+package com.acorn.test.tinkerdemo;
+
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import com.acorn.test.tinkerdemo.Log.MyLogImp;
+import com.acorn.test.tinkerdemo.util.SampleApplicationContext;
+import com.acorn.test.tinkerdemo.util.TinkerManager;
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.entry.DefaultApplicationLike;
+import com.tencent.tinker.lib.tinker.Tinker;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+import androidx.multidex.MultiDex;
+
+/**
+ * Created by acorn on 2021/11/10.
+ */
+@DefaultLifeCycle(application = "com.acorn.test.tinkerdemo.MyApplication",
+        flags = ShareConstants.TINKER_ENABLE_ALL,
+        loadVerifyFlag = false)
+public class MyApplicationLike extends DefaultApplicationLike {
+    public MyApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
+    }
+
+    /**
+     * install multiDex before install tinker
+     * so we don't need to put the tinker lib classes in the main dex
+     *
+     * @param base
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        //you must install multiDex whatever tinker is installed!
+        MultiDex.install(base);
+
+        SampleApplicationContext.application = getApplication();
+        SampleApplicationContext.context = getApplication();
+        TinkerManager.setTinkerApplicationLike(this);
+
+        TinkerManager.initFastCrashProtect();
+        //should set before tinker is installed
+        TinkerManager.setUpgradeRetryEnable(true);
+
+        //optional set logIml, or you can use default debug log
+        TinkerInstaller.setLogIml(new MyLogImp());
+
+        //installTinker after load multiDex
+        //or you can put com.tencent.tinker.** to main dex
+        TinkerManager.installTinker(this);
+        Tinker tinker = Tinker.with(getApplication());
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public void registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks callback) {
+        getApplication().registerActivityLifecycleCallbacks(callback);
+    }
+}
